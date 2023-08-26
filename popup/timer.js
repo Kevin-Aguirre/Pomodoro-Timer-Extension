1746aa58c50dbaf4ebfd0b62775aa1d6a7386f10
Kevin Aguirre <kevinaguirre463@gmail.com>
2023-08-25 23:21:27 -0400

    Created Pomodoro Timer, updated timer.css, timer.html, timer.js, added beep.wav

--- popup/timer.js
+++ popup/timer.js
@@ -1,96 +1,67 @@
 
-// /**
-// * Listen for clicks on the buttons, and send the appropriate message to
-// * the content script in the page.
-// */
-// function listenForClicks() {
-//   document.addEventListener("click", (e) => {
+document.addEventListener("DOMContentLoaded", function() {
+  let interval;
 
-//     /**
-//     * Given the name of a beast, get the URL to the corresponding image.
-//     */
-//     function beastNameToURL(beastName) {
-//       switch (beastName) {
-//         case "Frog":
-//           return browser.runtime.getURL("beasts/frog.jpg");
-//         case "Snake":
-//           return browser.runtime.getURL("beasts/snake.jpg");
-//         case "Turtle":
-//           return browser.runtime.getURL("beasts/turtle.jpg");
-//       }
-//     }
+  function formatTime(time) {
+    return time < 10 ? "0" + time: time; 
+  }
+  
+  function setCountdown(stateSeconds, onComplete) {
+    clearInterval(interval); // Clear any ongoing timer
+    console.log("i am now in the function setCountdown");
+    interval = setInterval(function() {
+      if (stateSeconds > 0) {
+        var minutes = Math.floor(stateSeconds / 60);
+        var seconds = stateSeconds % 60;
+        var timerDisplay = document.getElementById("timer");
+        timerDisplay.textContent = `${formatTime(minutes)}:${formatTime(seconds)}`;
+        stateSeconds--;
+      } else {
+        clearInterval(interval);
+        var timerDisplay = document.getElementById("timer");
+        timerDisplay.textContent = "00:00";
+        console.log(`the ${stateSeconds / 60} minutes are up!`);
+        new Audio('beep.wav').play();
+        if (onComplete) {
+          onComplete();
+        }
+      }
+    }, 1000);
+  }
 
-//     /**
-//     * Insert the page-hiding CSS into the active tab,
-//     * then get the beast URL and
-//     * send a "beastify" message to the content script in the active tab.
-//     */
-//     function beastify(tabs) {
-//       browser.tabs.insertCSS({ code: hidePage }).then(() => {
-//         const url = beastNameToURL(e.target.textContent);
-//         browser.tabs.sendMessage(tabs[0].id, {
-//           command: "beastify",
-//           beastURL: url
-//         });
-//       });
-//     }
+  let work_25_button_ref = document.getElementById("work_25_button");
+  let work_45_button_ref = document.getElementById("work_45_button");
+  let reset_button_ref = document.getElementById("reset");
 
-//     /**
-//     * Remove the page-hiding CSS from the active tab,
-//     * send a "reset" message to the content script in the active tab.
-//     */
-//     function reset(tabs) {
-//       browser.tabs.removeCSS({ code: hidePage }).then(() => {
-//         browser.tabs.sendMessage(tabs[0].id, {
-//           command: "reset",
-//         });
-//       });
-//     }
 
-//     /**
-//     * Just log the error to the console.
-//     */
-//     function reportError(error) {
-//       console.error(`Could not beastify: ${error}`);
-//     }
+  work_25_button_ref.addEventListener("click", function() {
+    let workMinutes = .10
+    let workSeconds = workMinutes * 60 
+    let restMinutes = .05
+    let restSeconds = restMinutes * 60 
 
-//     /**
-//     * Get the active tab,
-//     * then call "beastify()" or "reset()" as appropriate.
-//     */
-//     if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
-//       // Ignore when click is not on a button within <div id="popup-content">.
-//       return;
-//     }
-//     if (e.target.type === "reset") {
-//       browser.tabs.query({ active: true, currentWindow: true })
-//         .then(reset)
-//         .catch(reportError);
-//     } else {
-//       browser.tabs.query({ active: true, currentWindow: true })
-//         .then(beastify)
-//         .catch(reportError);
-//     }
-//   });
-// }
+    setCountdown(workSeconds, function() {
+      setCountdown(restSeconds);
+    })
+  });
+
+  work_45_button_ref.addEventListener("click", function() {
+    let workMinutes = .30
+    let workSeconds = workMinutes * 60 
+    let restMinutes = .15
+    let restSeconds = restMinutes * 60 
+
+    setCountdown(workSeconds, function() {
+      setCountdown(restSeconds);
+    })  
+  });
+
+  reset_button_ref.addEventListener("click", function() {
+    clearInterval(interval);
+    var timerDisplay = document.getElementById("timer");
+    timerDisplay.textContent = "00:00";
+  })
 
 
 
-/**
-* There was an error executing the script.
-* Display the popup's error message, and hide the normal UI.
-*/
-function reportExecuteScriptError(error) {
-  document.querySelector("#popup-content").classList.add("hidden");
-  document.querySelector("#error-content").classList.remove("hidden");
-  console.error(`Failed to execute beastify content script: ${error.message}`);
-}
-
-/**
-* When the popup loads, inject a content script into the active tab,
-* and add a click handler.
-* If we couldn't inject the script, handle the error.
-*/
-browser.tabs.executeScript({ file: "/content_scripts/beastify.js" })
-  .then(listenForClicks)
-  .catch(reportExecuteScriptError);
+});
